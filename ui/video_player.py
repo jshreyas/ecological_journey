1f249a4329f395f386e930a696ea6ae03174a143
Shreyas <jshreyas@users.noreply.github.com>
2025-05-19 21:58:54 -0700

    player window sizing issue (#30)
    * player window sizing issue
    
    * fixup

--- ui/video_player.py
+++ ui/video_player.py
@@ -4,9 +4,10 @@ from urllib.parse import urlparse, parse_qs
 
 class VideoPlayer:
 
-    def __init__(self, video_url: str, start: int = 0):
+    def __init__(self, video_url: str, start: int = 0, end: int = 1000000):
         self.video_id = self._extract_video_id(video_url)
         self.start = start
+        self.end = end
         self._render()
 
     def _extract_video_id(self, url: str) -> str:
@@ -20,14 +21,77 @@ class VideoPlayer:
             return url  # assume it's already a video ID
 
     def _render(self):
-        start_param = f"&start={self.start}" if self.start else ""
-        embed_url = f"https://www.youtube.com/embed/{self.video_id}?autoplay=1{start_param}"
-        ui.html(f'''
-            <iframe
-                src="{embed_url}"
-                style="width: 100%; height: 100%; border:10px solid white; position: absolute; top: 0; left: 0;"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen>
-            </iframe>
+        # Set explicit pixel size for the parent container
+        ui.html('''
+            <div id="yt-player-wrapper">
+                <div id="yt-player"></div>
+            </div>
+        ''')
+
+        # Load YouTube IFrame API
+        ui.add_head_html('''
+            <script src="https://www.youtube.com/iframe_api"></script>
+        ''')
+        # TODO: Make the height and width 100% resizable
+        # JS config and player logic
+        ui.run_javascript(f'''
+            window.ytConfig = {{
+                videoId: "{self.video_id}",
+                start: {self.start},
+                end: {self.end}
+            }};
+
+            let ytPlayer;
+            let ytEndInterval;
+
+            window.onYouTubeIframeAPIReady = function() {{
+                ytPlayer = new YT.Player('yt-player', {{
+                    height: '400',
+                    width: '700',
+                    videoId: window.ytConfig.videoId,
+                    playerVars: {{
+                        autoplay: 1,
+                        start: window.ytConfig.start
+                    }},
+                    events: {{
+                        'onReady': onPlayerReady,
+                        'onStateChange': onPlayerStateChange
+                    }}
+                }});
+            }};
+
+            function onPlayerReady(event) {{
+                event.target.playVideo();
+                if (ytEndInterval) clearInterval(ytEndInterval);
+                ytEndInterval = setInterval(() => {{
+                    const current = ytPlayer.getCurrentTime();
+                    if (current >= window.ytConfig.end) {{
+                        ytPlayer.pauseVideo();
+                        clearInterval(ytEndInterval);
+                    }}
+                }}, 500);
+            }}
+
+            function onPlayerStateChange(event) {{
+                // Optionally handle state changes
+            }}
+
+            window.setYTClip = function(start, end) {{
+                window.ytConfig.start = start;
+                window.ytConfig.end = end;
+                ytPlayer.seekTo(start, true);
+                ytPlayer.playVideo();
+                if (ytEndInterval) clearInterval(ytEndInterval);
+                ytEndInterval = setInterval(() => {{
+                    const current = ytPlayer.getCurrentTime();
+                    if (current >= window.ytConfig.end) {{
+                        ytPlayer.pauseVideo();
+                        clearInterval(ytEndInterval);
+                    }}
+                }}, 500);
+            }};
+
+            if (window.YT && window.YT.Player) {{
+                window.onYouTubeIframeAPIReady();
+            }}
         ''')
