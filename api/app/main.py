845a4e2863dee5fce3618d9495a76d83cec04156
Shreyas Jukanti <shreyas.jukanti@gmail.com>
2025-07-23 01:05:13 -0700

    test this

--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,10 @@
-# app/main.py
 import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
 from .routes import router
@@ -26,3 +27,5 @@ app.add_middleware(
 
 app.include_router(router)
 app.add_middleware(SessionMiddleware, secret_key=os.getenv("JWT_SECRET"))
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["ecological-journey-api.duckdns.org", "*.duckdns.org"])
+app.add_middleware(ProxyHeadersMiddleware)
