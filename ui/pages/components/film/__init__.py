3e7e999116d8902b019dc522d00f50138b6ce544
Shreyas Jukanti <shreyas.jukanti@gmail.com>
2026-06-10 14:38:13 -0700

    draft

--- ui/pages/components/film/__init__.py
+++ ui/pages/components/film/__init__.py
@@ -4,16 +4,12 @@ Film UI Components Package
 This package contains all the UI components used by the film page.
 """
 
-from .filmboard_tab import FilmboardTab
-from .navigation_tab import NavigationTab
 from .player_controls_tab import PlayerControlsTab
 from .share_dialog_tab import ShareDialogTab
 from .video_state import VideoState
 
 __all__ = [
     "VideoState",
-    "FilmboardTab",
-    "NavigationTab",
     "PlayerControlsTab",
     "ShareDialogTab",
 ]
