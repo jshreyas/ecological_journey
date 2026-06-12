14b3a01ec94d7f2eb9d9c9fedc86378dca8a5c5b
Shreyas Jukanti <shreyas.jukanti@gmail.com>
2026-06-12 14:24:57 -0700

    draft

--- ui/pages/components/film/timeline_tab.py
+++ ui/pages/components/film/timeline_tab.py
@@ -5,7 +5,7 @@ from ui.utils.utils import format_time
 from .video_state import VideoState
 
 
-# TODO: TimelineTab is not in sync with MatadataTab play buttons
+# TODO: TimelineTab and MatadataTab have minor sync bugs when clips are present
 class TimelineTab:
 
     def __init__(
